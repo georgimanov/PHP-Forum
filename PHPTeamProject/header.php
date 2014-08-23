4280c5a7472e35c64bc321af7df99fea94f76984
momchilDishev <mo.dishev@gmail.com>
2014-08-23 16:39:12 +0300

    Insert html into file

---
+++ PHPTeamProject/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<meta charset="utf-8" />
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title> Soupaholics</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Forum</a></li>
+                <li><a href="#">Contacts</a></li>
+            </ul>
+        </nav>
+    </header>
