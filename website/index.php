218e7e21d847baaf80ece2fdd3e685033fce2ae8
Dsmilyanov <dsmilyanov@gmail.com>
2014-08-25 19:39:01 +0300

    index.php added

---
+++ website/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Soupaholic</title>
+    <link rel="stylesheet" href="styles/index-styles.css">
+</head>
+<body>
+<main>
+
+    <!-- <div id="global-wrapper"> -->
+      <div id="index-wrapper">
+        <a href="login.php" id="login">LOGIN</a>
+        <img id="logo" src="images/logo-fulll.png">
+        <a href="register.php" id="register">REGISTER</a>
+      </div>
+    <!-- </div> -->
+</main>
+<footer></footer>
+
+</body>
+</html>
